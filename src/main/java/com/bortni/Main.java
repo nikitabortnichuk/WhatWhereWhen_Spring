5dd26d5b37ae3431e134b1f725553b18dc4b301a
nikitabortnichuk <nikita.bortnichuk41@gmail.com>
2020-07-05 19:05:33 +0300

    some fixes

--- src/main/java/com/bortni/Main.java
+++ src/main/java/com/bortni/Main.java
@@ -1,12 +1,16 @@
 package com.bortni;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Main {
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
+        LOGGER.info("Application started");
         SpringApplication.run(Main.class, args);
     }
 
